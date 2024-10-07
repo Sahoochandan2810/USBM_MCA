887bf474e1f31266d198c480c3a0726c32e1e496
Chandan Kumar Sahoo <163462650+Sahoochandan2810@users.noreply.github.com>
2024-10-07 12:29:54 +0530

    Add files via upload

---
+++ MERN STACK/frontend/javascript/class2/string.js
@@ -0,0 +1,13 @@
+//string
+
+let str = "Chandan";
+console.log(str);
+
+let name = "Chandan";
+let age = 23;
+let address = "Bhubaneswar";
+
+// Use backticks for template literals
+console.log(`My name is: ${name}, age is: ${age}, address is: ${address}`);
+
+console.log(address.length);
