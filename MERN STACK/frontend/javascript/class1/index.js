623652d893453b9a99b2df1d9403fbd10369f5cd
Chandan Kumar Sahoo <163462650+Sahoochandan2810@users.noreply.github.com>
2024-10-07 11:49:45 +0530

    Add files via upload

---
+++ MERN STACK/frontend/javascript/class1/index.js
@@ -0,0 +1,14 @@
+console.log("Hello Chandan");
+
+let a = 10;
+
+var b = 20;
+const c = 30;
+
+const city = "Bhubaneswar";
+console.log(address);
+var address = "Bhubaneswar";
+
+console.log(address);
+
+console.log(c);
